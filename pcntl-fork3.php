8099b9ec083d5406dee0d7caae168bcd94916ee4
jinghewang <jinghewang@163.com>
2016-08-28 11:54:10 +0800

    fork3

--- pcntl-fork3.php
+++ pcntl-fork3.php
@@ -23,11 +23,17 @@ function dummy_business(){
     $conn = mysqli_connect($host, $user, $password) or die(mysqli_error());
     mysqli_select_db($conn, $db);
     for ($i = 0; $i < 10000; $i++) {
+        mysqli_query($conn, 'BEGIN');
         $rs = mysqli_query($conn, 'SELECT num FROM rp_counter WHERE id = 1');
         //mysqli_free_result($rs);
         $row = mysqli_fetch_array($rs);
         $num = $row[0];
-        mysqli_query($conn, "UPDATE rp_counter SET num = {$num} + 1 WHERE id = 1");
+        mysqli_query($conn, 'UPDATE rp_counter SET num = '.$num.' + 1 WHERE id = 1');
+        if(mysqli_errno($conn)) {
+            mysqli_query($conn, 'ROLLBACK');
+        } else {
+            mysqli_query($conn, 'COMMIT');
+        }
     }
     mysqli_close($conn);
 }
